28f31a5286712212b8d1a5900ab36fa77dda2208
Martin Bachmann <martinpezbach@gmail.com>
2022-08-26 17:49:17 -0300

    Requisito 4

--- src/routes/loginRoutes.js
+++ src/routes/loginRoutes.js
@@ -3,7 +3,38 @@ const generateToken = require('../services/generateToken');
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+  const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (!email) {
+    return res.status(400).json({
+      message: 'O campo "email" é obrigatório',
+    }); 
+  }
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({
+      message: 'O "email" deve ter o formato "email@email.com"',
+    });
+  }
+  next();
+};
+
+const validatePassword = (req, res, next) => {
+  const { password } = req.body;
+  if (!password) {
+    return res.status(400).json({
+      message: 'O campo "password" é obrigatório',
+    });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({
+      message: 'O "password" deve ter pelo menos 6 caracteres',
+    });
+  }
+  next();
+};
+
+router.post('/', validateEmail, validatePassword, async (req, res) => {
   const token = generateToken();
   return res.status(200).json({
     token,
